61cd435f53ce49eada52fa02ff682e3fbcf94bed
LipikaGarg <garglipika.98@gmail.com>
2022-01-14 15:11:40 -0600

    Modified Makefile
    modified paths to makefile

--- Makefile.mk
+++ Makefile.mk
@@ -1,12 +1,12 @@
-TC_PATH = /usr
+TC_PATH = /Users/lipika/Documents/Lipika/github_repositories/MiBench2/gcc-arm-none-eabi-6-2017-q2-update
 TOOLCHAIN = arm-none-eabi
 ARMGNU = $(TC_PATH)/bin/$(TOOLCHAIN)
-CC = $ arm-none-eabi-gcc
+CC = $(TC_PATH)/bin/arm-none-eabi-gcc
 CHIP = cortex-m0plus
 OPTLVL = -O3 
 
 #FLAGS = -DBARE_METAL -Wall $(OPTLVL) -target $(TOOLCHAIN) -mcpu=$(CHIP) -mthumb --specs=nosys.specs -nostartfiles -ffreestanding -std=c99 -fomit-frame-pointer -fno-optimize-sibling-calls #-static
-FLAGS = -DBARE_METAL -Wall $(OPTLVL) -march=armv6-m -mcpu=$(CHIP) -mthumb -msoft-float --specs=nosys.specs -nostartfiles -ffreestanding -std=c99 -fomit-frame-pointer -fno-optimize-sibling-calls 
+FLAGS = -DBARE_METAL -Wall $(OPTLVL) -march=armv6s-m -mcpu=$(CHIP) -mthumb -msoft-float --specs=nosys.specs -nostartfiles -ffreestanding -std=c99 -fomit-frame-pointer -fno-optimize-sibling-calls 
 #LIBS = --start-group -lm -lc -lbuiltins --end-group
 LIBS = -lnosys -lc -lm
 OBJS := vectors.o putget.o supportFuncs.o LED.o $(OBJS)
